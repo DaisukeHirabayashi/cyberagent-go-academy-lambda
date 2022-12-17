446290b79ce930eee2c38d9c6c4cbfa98344e73e
DaisukeHirabayashi <daisuke@pluslab.org>
2022-12-17 13:58:31 +0900

    dbのpackageの追加

---
+++ app/db/db.go
@@ -0,0 +1,34 @@
+package db
+
+import (
+	"log"
+
+	"github.com/jinzhu/gorm"
+	_ "github.com/jinzhu/gorm/dialects/postgres"
+)
+
+var (
+	db  *gorm.DB
+	err error
+)
+
+func Init() {
+	log.Print("DB init...")
+	// TODO : os.Getenv("DATABASE_URL") does not work...
+	databaseUrl := "academy08:78fqoLQpJ9XXpAab8fVuQmbzdmhACZ@(ca-academy-db.c9ml7do7yvmn.ap-northeast-1.rds.amazonaws.com:3306)/academy08"
+	db, err = gorm.Open("mysql", databaseUrl)
+	if err != nil {
+		panic(err)
+	}
+	log.Print("DB connection success!")
+}
+
+func GetDB() *gorm.DB {
+	return db
+}
+
+func Close() {
+	if err := db.Close(); err != nil {
+		panic(err)
+	}
+}
--- app/go.mod
+++ app/go.mod
@@ -1,4 +1,7 @@
-require github.com/aws/aws-lambda-go v1.23.0
+require (
+	github.com/aws/aws-lambda-go v1.23.0
+	github.com/jinzhu/gorm v1.9.16
+)
 
 replace gopkg.in/yaml.v2 => gopkg.in/yaml.v2 v2.2.8
 

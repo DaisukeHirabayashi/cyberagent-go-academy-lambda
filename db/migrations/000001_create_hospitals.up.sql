1a79c76872a5288acbb578f8115af62498e17918
DaisukeHirabayashi <daisuke@pluslab.org>
2022-12-18 13:13:46 +0900

    migrationファイルの追加

---
+++ db/migrations/000001_create_hospitals.up.sql
@@ -0,0 +1,17 @@
+CREATE TABLE `hospitals` (
+    `id` INT AUTO_INCREMENT NOT NULL PRIMARY KEY,
+    `facility_id` INT,
+    `facility_name` VARCHAR(255),
+    `zip_cod` VARCHAR(255),
+    `pref_name` VARCHAR(255),
+    `facility_addr` VARCHAR(255),
+    `facility_tel` VARCHAR(255),
+    `facility_type` VARCHAR(255),
+    `ans_type` VARCHAR(255),
+    `local_gov_code` VARCHAR(255),
+    `city_name` VARCHAR(255),
+    `facility_code` VARCHAR(255),
+    `submit_date` TIMESTAMP NOT NULL,
+    `created_at` TIMESTAMP NOT NULL DEFAULT CURRENT_TIMESTAMP,
+    `updated_at` TIMESTAMP NOT NULL DEFAULT CURRENT_TIMESTAMP
+);

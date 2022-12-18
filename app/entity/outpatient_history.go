22dfd76805cf7dd1aec3440a418df682f1503cf8
DaisukeHirabayashi <daisuke@pluslab.org>
2022-12-18 18:01:57 +0900

    外来履歴のdaoの追加

---
+++ app/entity/outpatient_history.go
@@ -0,0 +1,21 @@
+package entity
+
+import "time"
+
+type OutpatientHistory struct {
+	Id           uint      `json:"id"`
+	FacilityId   uint      `json:"facility_id"`
+	FacilityName string    `json:"facility_name"`
+	ZipCode      string    `json:"zip_code"`
+	PrefName     string    `json:"pref_name"`
+	FacilityAddr string    `json:"facility_addr"`
+	FacilityTel  string    `json:"facility_tel"`
+	SubmitDate   time.Time `json:"submit_date"`
+	FacilityType string    `json:"facility_type"`
+	AnsType      string    `json:"ans_type"`
+	LocalGovCode string    `json:"local_gov_code"`
+	CityName     string    `json:"city_name"`
+	FaciltyCode  string    `json:"facility_code"`
+	CreatedAt    time.Time `json:"created_at"`
+	UpdatedAt    time.Time `json:"updated_at"`
+}

5508435457eb907b08493d05aee39b436bcbaf20
DaisukeHirabayashi <daisuke@pluslab.org>
2022-12-30 15:50:32 +0900

    returnの変更

--- app/service/hospital_service.go
+++ app/service/hospital_service.go
@@ -2,7 +2,6 @@ package service
 
 import (
 	"encoding/json"
-	"log"
 	"time"
 
 	"github.com/DaisukeHirabayashi/cyberagent-go-academy-lambda/client"
@@ -10,12 +9,10 @@ import (
 )
 
 // 前日の履歴を取ってくる
-func GetLastDayOutpatientHistory() error {
+func GetLastDayOutpatientHistory() []dao.Hospital {
 	time := time.Now().Format("20060102")
 	reponse_body, _ := client.GetMedicalSystem(&time)
 	var hospitals []dao.Hospital
 	json.Unmarshal(reponse_body, &hospitals)
-	log.Println("hospital:", hospitals[0])
-
-	return nil
+	return hospitals
 }

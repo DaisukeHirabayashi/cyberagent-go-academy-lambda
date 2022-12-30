3a9ca1b7f1958af23893028b94f2e633ac5c3377
DaisukeHirabayashi <daisuke@pluslab.org>
2022-12-30 16:35:39 +0900

    わかりやすさのために修正

--- app/mapper/outpatient_mapper.go
+++ app/mapper/outpatient_mapper.go
@@ -14,13 +14,13 @@ func HospitalDaosToOutpatientHistorys(dao_hospitals []dao.Hospital) ([]entity.Ou
 	for _, dao_hospital := range dao_hospitals {
 		facility_id, err := strconv.ParseUint(dao_hospital.FacilityId, 10, 64)
 		if err != nil {
-			log.Println("Error:", err)
+			log.Println("Error:", err, dao_hospital)
 			return nil, err
 		}
 		submit_date, err := time.Parse("2006-01-02", dao_hospital.SubmitDate)
 
 		if err != nil {
-			log.Println("Error:", err)
+			log.Println("Error:", err, dao_hospital)
 			return nil, err
 		}
 		entity_hospital := entity.OutpatientHistory{FacilityId: uint(facility_id), FacilityName: dao_hospital.FacilityName, ZipCode: dao_hospital.ZipCode, PrefName: dao_hospital.PrefName, FacilityAddr: dao_hospital.FacilityAddr, FacilityTel: dao_hospital.FacilityTel, SubmitDate: submit_date, FacilityType: dao_hospital.FacilityType, AnsType: dao_hospital.AnsType, LocalGovCode: dao_hospital.LocalGovCode, CityName: dao_hospital.CityName, FaciltyCode: dao_hospital.FaciltyCode}

2e19cea0c8a43f03cde3898af82efd42acdc8063
DaisukeHirabayashi <daisuke@pluslab.org>
2022-12-31 16:00:36 +0900

    errorの追加

--- app/main.go
+++ app/main.go
@@ -1,6 +1,7 @@
 package main
 
 import (
+	"encoding/json"
 	"errors"
 	"net/http"
 	"net/url"
@@ -8,6 +9,10 @@ import (
 	"github.com/DaisukeHirabayashi/cyberagent-go-academy-lambda/service"
 	"github.com/aws/aws-lambda-go/lambda"
 )
+
+type payload struct {
+	Text string `json:"text"`
+}
 
 var (
 	// DefaultHTTPGetAddress Default Address
@@ -21,16 +26,18 @@ var (
 )
 
 func OnlyErrors() error {
-	// service.GetBooks()
+	//service.GetBooks()
 	outpatientHistories, err := service.GetLastDayOutpatientHistory()
 	if err != nil {
-		http.PostForm("https://hooks.slack.com/services/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", url.Values{"payload": {string(err.Error())}})
+		err_json, _ := json.Marshal(payload{Text: err.Error()})
+		http.PostForm("https://hooks.slack.com/services/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", url.Values{"payload": {string(err_json)}})
 	}
 
 	err = service.CreateOutpatientHistoires(outpatientHistories)
 
 	if err != nil {
-		http.PostForm("https://hooks.slack.com/services/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", url.Values{"payload": {string(err.Error())}})
+		err_json, _ := json.Marshal(payload{Text: err.Error()})
+		http.PostForm("https://hooks.slack.com/services/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", url.Values{"payload": {string(err_json)}})
 	}
 	return nil
 }

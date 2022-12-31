b91cf3cace376e4898527abb85a8db5a6d028294
DaisukeHirabayashi <daisuke@pluslab.org>
2022-12-31 16:01:46 +0900

    errorの追加

--- app/main.go
+++ app/main.go
@@ -29,14 +29,14 @@ func OnlyErrors() error {
 	//service.GetBooks()
 	outpatientHistories, err := service.GetLastDayOutpatientHistory()
 	if err != nil {
-		err_json, _ := json.Marshal(payload{Text: err.Error()})
+		err_json, _ := json.Marshal(payload{Text: "error GetLastDayOutpatientHistory:" + err.Error()})
 		http.PostForm("https://hooks.slack.com/services/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", url.Values{"payload": {string(err_json)}})
 	}
 
 	err = service.CreateOutpatientHistoires(outpatientHistories)
 
 	if err != nil {
-		err_json, _ := json.Marshal(payload{Text: err.Error()})
+		err_json, _ := json.Marshal(payload{Text: "error CreateOutpatientHistoires:" + err.Error()})
 		http.PostForm("https://hooks.slack.com/services/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", url.Values{"payload": {string(err_json)}})
 	}
 	return nil

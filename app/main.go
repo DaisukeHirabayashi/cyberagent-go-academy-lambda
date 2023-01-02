53d2786efd34fa1ef157cf8fc34b6f6320720ca2
DaisukeHirabayashi <daisuke@pluslab.org>
2023-01-02 13:56:59 +0900

    余分コードをなくす

--- app/main.go
+++ app/main.go
@@ -1,26 +1,10 @@
 package main
 
 import (
-	"errors"
 	"net/http"
 
 	"github.com/DaisukeHirabayashi/cyberagent-go-academy-lambda/service"
 	"github.com/aws/aws-lambda-go/lambda"
-)
-
-type payload struct {
-	Text string `json:"text"`
-}
-
-var (
-	// DefaultHTTPGetAddress Default Address
-	DefaultHTTPGetAddress = "https://checkip.amazonaws.com"
-
-	// ErrNoIP No IP found in response
-	ErrNoIP = errors.New("No IP in HTTP response")
-
-	// ErrNon200Response non 200 status code in response
-	ErrNon200Response = errors.New("Non 200 Response found")
 )
 
 func OnlyErrors() error {
--- slack/main.go
+++ slack/main.go
@@ -2,23 +2,11 @@ package main
 
 import (
 	"encoding/json"
-	"errors"
 	"net/http"
 	"net/url"
 
 	"github.com/aws/aws-lambda-go/events"
 	"github.com/aws/aws-lambda-go/lambda"
-)
-
-var (
-	// DefaultHTTPGetAddress Default Address
-	DefaultHTTPGetAddress = "https://checkip.amazonaws.com"
-
-	// ErrNoIP No IP found in response
-	ErrNoIP = errors.New("No IP in HTTP response")
-
-	// ErrNon200Response non 200 status code in response
-	ErrNon200Response = errors.New("Non 200 Response found")
 )
 
 type payload struct {

c58029e38b9c990597d952b84b5f8b4436e2a514
DaisukeHirabayashi <daisuke@pluslab.org>
2023-01-02 23:38:20 +0900

    コメントの削除

--- app/main.go
+++ app/main.go
@@ -11,16 +11,12 @@ func CreateOutpatientHistoires() error {
 	outpatientHistories, err := service.GetLastDayOutpatientHistory()
 	if err != nil {
 		go http.Get("http://127.0.0.1:3000/notification?message=" + "error GetLastDayOutpatientHistory:" + err.Error())
-		// err_json, _ := json.Marshal(payload{Text: "error GetLastDayOutpatientHistory:" + err.Error()})
-		// http.PostForm("https://hooks.slack.com/services/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", url.Values{"payload": {string(err_json)}})
 	}
 
 	err = service.CreateOutpatientHistoires(outpatientHistories)
 
 	if err != nil {
 		go http.Get("http://127.0.0.1:3000/notification?message=" + "error GetLastDayOutpatientHistory:" + err.Error())
-		// err_json, _ := json.Marshal(payload{Text: "error CreateOutpatientHistoires:" + err.Error()})
-		// http.PostForm("https://hooks.slack.com/services/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", url.Values{"payload": {string(err_json)}})
 	}
 	return nil
 }

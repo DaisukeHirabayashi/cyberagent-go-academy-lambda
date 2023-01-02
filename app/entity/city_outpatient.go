7af9a93626acfd861c9359764286247cba2df795
DaisukeHirabayashi <daisuke@pluslab.org>
2023-01-02 23:13:56 +0900

    city patientの追加

---
+++ app/entity/city_outpatient.go
@@ -0,0 +1,13 @@
+package entity
+
+import "time"
+
+type CityPatient struct {
+	Id         uint      `json:"id"`
+	PrefName   string    `json:"pref_name"`
+	CityName   string    `json:"city_name"`
+	Number     uint      `json:"number"`
+	SubmitDate time.Time `json:"submit_date"`
+	CreatedAt  time.Time `json:"created_at"`
+	UpdatedAt  time.Time `json:"updated_at"`
+}

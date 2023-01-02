a30291ec481828f04df05e16cb8e32b579899ab5
DaisukeHirabayashi <daisuke@pluslab.org>
2023-01-02 13:54:46 +0900

    slack notificationするように変更

--- app/main.go
+++ app/main.go
@@ -1,10 +1,8 @@
 package main
 
 import (
-	"encoding/json"
 	"errors"
 	"net/http"
-	"net/url"
 
 	"github.com/DaisukeHirabayashi/cyberagent-go-academy-lambda/service"
 	"github.com/aws/aws-lambda-go/lambda"
@@ -29,15 +27,17 @@ func OnlyErrors() error {
 	//service.GetBooks()
 	outpatientHistories, err := service.GetLastDayOutpatientHistory()
 	if err != nil {
-		err_json, _ := json.Marshal(payload{Text: "error GetLastDayOutpatientHistory:" + err.Error()})
-		http.PostForm("https://hooks.slack.com/services/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", url.Values{"payload": {string(err_json)}})
+		go http.Get("http://127.0.0.1:3000/notification?message=" + "error GetLastDayOutpatientHistory:" + err.Error())
+		// err_json, _ := json.Marshal(payload{Text: "error GetLastDayOutpatientHistory:" + err.Error()})
+		// http.PostForm("https://hooks.slack.com/services/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", url.Values{"payload": {string(err_json)}})
 	}
 
 	err = service.CreateOutpatientHistoires(outpatientHistories)
 
 	if err != nil {
-		err_json, _ := json.Marshal(payload{Text: "error CreateOutpatientHistoires:" + err.Error()})
-		http.PostForm("https://hooks.slack.com/services/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", url.Values{"payload": {string(err_json)}})
+		go http.Get("http://127.0.0.1:3000/notification?message=" + "error GetLastDayOutpatientHistory:" + err.Error())
+		// err_json, _ := json.Marshal(payload{Text: "error CreateOutpatientHistoires:" + err.Error()})
+		// http.PostForm("https://hooks.slack.com/services/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", url.Values{"payload": {string(err_json)}})
 	}
 	return nil
 }

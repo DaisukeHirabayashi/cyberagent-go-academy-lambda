e7b9581f386812b888b84ad345df3ec1b206d5c5
DaisukeHirabayashi <daisuke@pluslab.org>
2023-01-02 23:06:58 +0900

    create_city_outpatientsの追加

---
+++ db/migrations/000003_create_city_outpatients.up.sql
@@ -0,0 +1,11 @@
+CREATE TABLE `city_outpatients` (
+    `id` INT AUTO_INCREMENT NOT NULL PRIMARY KEY,
+    `pref_name` VARCHAR(255),
+    `city_name` VARCHAR(255),
+    `count` INT,
+    `submit_date` TIMESTAMP NOT NULL,
+    `created_at` TIMESTAMP NOT NULL DEFAULT CURRENT_TIMESTAMP,
+    `updated_at` TIMESTAMP NOT NULL DEFAULT CURRENT_TIMESTAMP,
+    index index_city_outpatients_on_pref_name_and_city_name_and_su_date (pref_name, city_name, submit_date),
+    index index_city_outpatients_on_city_name_and_submit_date (city_name, submit_date)
+);

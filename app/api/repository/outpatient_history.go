f40b36d8688688cf930138aae2011de2e693c30a
DaisukeHirabayashi <daisuke@pluslab.org>
2023-01-07 15:04:10 +0900

    市で絞り込みができるように調整

--- app/api/repository/outpatient_history.go
+++ app/api/repository/outpatient_history.go
@@ -12,3 +12,12 @@ func SearchByPrefName(pref_name string) func(db *gorm.DB) *gorm.DB {
 		return db
 	}
 }
+
+func SearchByCityName(city_name string) func(db *gorm.DB) *gorm.DB {
+	return func(db *gorm.DB) *gorm.DB {
+		if city_name != "" {
+			db.Where("city_name=?", city_name)
+		}
+		return db
+	}
+}
--- app/service/city_outoatient.go
+++ app/service/city_outoatient.go
@@ -10,9 +10,10 @@ import (
 func GetCityPatients(request events.APIGatewayProxyRequest) ([]entity.City_Outpatient, error) {
 	db := db.Init()
 	pref_name := request.QueryStringParameters["pref_name"]
+	city_name := request.QueryStringParameters["city_name"]
 
 	var city_outpatients []entity.City_Outpatient
-	if err := db.Scopes(repository.SearchByPrefName(pref_name)).Find(&city_outpatients).Error; err != nil {
+	if err := db.Scopes(repository.SearchByPrefName(pref_name)).Scopes(repository.SearchByCityName(city_name)).Find(&city_outpatients).Error; err != nil {
 		return city_outpatients, err
 	}
 

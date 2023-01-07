37b3c17518b2931382216cb0dce6f16a081eafdc
DaisukeHirabayashi <daisuke@pluslab.org>
2023-01-07 13:12:17 +0900

    ulrの変更

--- app/batch/create_city_patient/main.go
+++ app/batch/create_city_patient/main.go
@@ -8,12 +8,12 @@ import (
 )
 
 func CreateOutpatientHistoires() error {
-	go http.Get("https://a5pky2m9q6.execute-api.ap-northeast-1.amazonaws.com/Prod/notification?message=:rocket:start_batch_for_create_last_day_outpatient_hisotory")
+	go http.Get("https://wgmjrdremf.execute-api.ap-northeast-1.amazonaws.com/Prod/notification?message=:rocket:start_batch_for_create_last_day_outpatient_hisotory")
 	err := service.CreateCityOutPatients()
 	if err != nil {
-		go http.Get("https://a5pky2m9q6.execute-api.ap-northeast-1.amazonaws.com/Prod/notification?message=" + "error GetLastDayOutpatientHistory:" + err.Error())
+		go http.Get("https://wgmjrdremf.execute-api.ap-northeast-1.amazonaws.com/Prod/notification?message=" + "error GetLastDayOutpatientHistory:" + err.Error())
 	}
-	go http.Get("https://a5pky2m9q6.execute-api.ap-northeast-1.amazonaws.com/Prod/notification?message=:tada:finish_batch_for_create_last_day_outpatient_hisotory")
+	go http.Get("https://wgmjrdremf.execute-api.ap-northeast-1.amazonaws.com/Prod/notification?message=:tada:finish_batch_for_create_last_day_outpatient_hisotory")
 	return nil
 }
 
--- app/main.go
+++ app/main.go
@@ -8,20 +8,20 @@ import (
 )
 
 func CreateOutpatientHistoires() error {
-	go http.Get("https://a5pky2m9q6.execute-api.ap-northeast-1.amazonaws.com/Prod/notification?message=" + ":rocket:start_batch_for_create_last_day_city_patient")
+	go http.Get("https://wgmjrdremf.execute-api.ap-northeast-1.amazonaws.com/Prod/notification?message=" + ":rocket:start_batch_for_create_last_day_city_patient")
 
 	outpatientHistories, err := service.GetLastDayOutpatientHistory()
 	if err != nil {
-		go http.Get("https://a5pky2m9q6.execute-api.ap-northeast-1.amazonaws.com/Prod/notification?message=" + "error GetLastDayOutpatientHistory:" + err.Error())
+		go http.Get("https://wgmjrdremf.execute-api.ap-northeast-1.amazonaws.com/Prod/notification?message=" + "error GetLastDayOutpatientHistory:" + err.Error())
 	}
 
 	err = service.CreateOutpatientHistoires(outpatientHistories)
 
 	if err != nil {
-		go http.Get("https://a5pky2m9q6.execute-api.ap-northeast-1.amazonaws.com/Prod/notification?message=" + "error GetLastDayOutpatientHistory:" + err.Error())
+		go http.Get("https://wgmjrdremf.execute-api.ap-northeast-1.amazonaws.com/Prod/notification?message=" + "error GetLastDayOutpatientHistory:" + err.Error())
 	}
 
-	go http.Get("https://a5pky2m9q6.execute-api.ap-northeast-1.amazonaws.com/Prod/notification?message=" + ":tada:finish_batch_for_create_last_day_city_patient")
+	go http.Get("https://wgmjrdremf.execute-api.ap-northeast-1.amazonaws.com/Prod/notification?message=" + ":tada:finish_batch_for_create_last_day_city_patient")
 	return nil
 }
 

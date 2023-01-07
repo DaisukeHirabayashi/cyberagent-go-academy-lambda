132ffc3d8f9ad0bb550b82848ac0531064d26538
DaisukeHirabayashi <daisuke@pluslab.org>
2023-01-07 10:58:14 +0900

    slack通知の追加

--- app/batch/create_city_patient/main.go
+++ app/batch/create_city_patient/main.go
@@ -8,10 +8,12 @@ import (
 )
 
 func CreateOutpatientHistoires() error {
+	go http.Get("https://a5pky2m9q6.execute-api.ap-northeast-1.amazonaws.com/Prod/notification?message=" + "患者数のデータ整形開始")
 	err := service.CreateCityOutPatients()
 	if err != nil {
 		go http.Get("http://127.0.0.1:3000/notification?message=" + "error GetLastDayOutpatientHistory:" + err.Error())
 	}
+	go http.Get("https://a5pky2m9q6.execute-api.ap-northeast-1.amazonaws.com/Prod/notification?message=" + "患者数のデータ整形終了")
 	return nil
 }
 

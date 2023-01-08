b57d7f22b492b1cbf310ac6653f4f96523929fa7
DaisukeHirabayashi <daisuke@pluslab.org>
2023-01-08 14:39:09 +0900

    testの追加

--- app/main_test.go
+++ app/main_test.go
@@ -1,18 +1,32 @@
 package main
 
 import (
+	"io/ioutil"
+	"log"
+	"os"
 	"testing"
 	"time"
 
 	"github.com/jarcoal/httpmock"
+	"github.com/joho/godotenv"
 )
+
+func TestMain(m *testing.M) {
+	godotenv.Load(".env.test")
+	exitVal := m.Run()
+	log.Println("Do stuff AFTER the tests!")
+
+	os.Exit(exitVal)
+}
 
 func TestHandler(t *testing.T) {
 	t.Run("Unable to get IP", func(t *testing.T) {
 		time := time.Now().AddDate(0, 0, -1).Format("20060102")
+		httpmock.Activate()
 		defer httpmock.DeactivateAndReset()
-		httpmock.RegisterResponder("GET", "https://opendata.corona.go.jp/"+time,
-			httpmock.NewStringResponder(200, "mocked"),
+		mock_json, _ := ioutil.ReadFile("../test_data/20221230.json")
+		httpmock.RegisterResponder("GET", "https://opendata.corona.go.jp/api/covid19DailySurvey/"+time,
+			httpmock.NewStringResponder(200, string(mock_json)),
 		)
 		err := CreateOutpatientHistoires()
 		if err == nil {

56d6542d013c49bc2402c1298ef88c5d00d85582
DaisukeHirabayashi <daisuke@pluslab.org>
2023-01-14 02:26:16 +0900

    関数名の変更

---
+++ app/grpc/get_outpatinet_histories.go
@@ -0,0 +1,33 @@
+package main
+
+import (
+	"fmt"
+	"log"
+
+	"github.com/DaisukeHirabayashi/cyberagent-go-academy-lambda/client"
+	"github.com/DaisukeHirabayashi/cyberagent-go-academy-lambda/mapper"
+	pb "github.com/DaisukeHirabayashi/cyberagent-go-academy-lambda/proto"
+	"github.com/DaisukeHirabayashi/cyberagent-go-academy-lambda/service"
+	"google.golang.org/grpc/codes"
+	"google.golang.org/grpc/status"
+	"google.golang.org/protobuf/types/known/emptypb"
+)
+
+func (*Server) GetOutpatientHistories(_ *emptypb.Empty, stream pb.OutpatientHistoryService_GetOutpatientHistoriesServer) error {
+	log.Println("ListBlogs was invoked")
+
+	service := service.HospitalService{Client: &client.CoronaClient{}}
+	outpatinet_histories, err := service.GetOutpatientHistories()
+	if err != nil {
+		return status.Errorf(
+			codes.Internal,
+			fmt.Sprintf("Unknown internal error: %v", err),
+		)
+	}
+
+	for _, outpatinet_history := range outpatinet_histories {
+		stream.Send(mapper.OutpatientHistoryEntityToPbOutpatientHistory(outpatinet_history))
+	}
+
+	return nil
+}
